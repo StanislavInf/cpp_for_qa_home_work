1c5b732974dd1e1cb2d5c9d66455549c96e07b6a
Dyachenko Stanislav <dyachenko.stas.official@gmail.com>
2024-09-28 10:18:44 +0300

    FSPS-1 Реализовать пример из семинаров

--- main.cpp
+++ main.cpp
@@ -2,21 +2,21 @@
 #include <cstdlib>
 #include "square.h"
 // int argc, char *argv[]
-int main () {
-//    std::cout << "Hello World!";
-//    if (argc != 3) {
-//        std::cerr << "Provide exactly 2 arguments\n";
-//        return 1;
-//    }
-//    int a = std::atoi(argv[1]);
-//    int b = std::atoi(argv[2]);
-    //    std::cout << square(a, b) << '\n';
-    int a;
-    int b;
-    std::cin >> a >> b;
-    if (a == 0 || b == 0) {
-        return 0;
+int main (int argc, char *argv[]) {
+    std::cout << "Hello World!";
+    if (argc != 3) {
+        std::cerr << "Provide exactly 2 arguments\n";
+        return 1;
     }
-    std::cout << square(a, b);
+    int a = std::atoi(argv[1]);
+    int b = std::atoi(argv[2]);
+    std::cout << square(a, b) << '\n';
+//    int a;
+//    int b;
+//    std::cin >> a >> b;
+//    if (a == 0 || b == 0) {
+//        return 0;
+//    }
+//    std::cout << square(a, b);
     return 0;
 }
