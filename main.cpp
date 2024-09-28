fa374c8960424dbb0e40469858f9974b49e96dae
Dyachenko Stanislav <dyachenko.stas.official@gmail.com>
2024-09-28 09:55:37 +0300

    FSPS-1 Реализовать пример из семинаров

--- main.cpp
+++ main.cpp
@@ -2,12 +2,13 @@
 #include "square.h"
 
 int main() {
-    int a;
-    int b;
-    std::cin >> a >> b;
-    if (a == 0 || b == 0) {
-        return 0;
-    }
-    std::cout << square(a, b);
+    std::cout << "Hello Word!";
+//    int a;
+//    int b;
+//    std::cin >> a >> b;
+//    if (a == 0 || b == 0) {
+//        return 0;
+//    }
+//    std::cout << square(a, b);
     return 0;
 }
