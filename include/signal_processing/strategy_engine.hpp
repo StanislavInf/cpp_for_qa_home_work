3f6c9da9733872728017464582ccd6f611447088
Dyachenko Stanislav <dyachenko.stas.official@gmail.com>
2024-09-29 21:41:31 +0300

    FSPS-2 Создать и описать архитектуру

---
+++ include/signal_processing/strategy_engine.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#include <vector>
+
+class StrategyEngine {
+public:
+    // Простая реализация скользящего среднего
+    static double movingAverage(const std::vector<double>& prices, size_t period);
+
+};
